0a6245eeb82ec2a302b013d03dd64a6bf2f4753e
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-05-17 17:21:56 +0530

    backend added

---
+++ Task-2-Payment Getway Integration/Backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1);
+  });
+
+const port = process.env.PORT || 8082;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
